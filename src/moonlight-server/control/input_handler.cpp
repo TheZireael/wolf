be971c187642216866ed8f4124ec73e2f8f60d21
TheZireael <gthub@omtrhn.com>
2026-02-10 15:43:04 +0300

    Add ULTIMATE2 controller type and update version
    Updated the version number for the 8BitDo Ultimate 2 joypad and added support for the ULTIMATE2 controller type.

--- src/moonlight-server/control/input_handler.cpp
+++ src/moonlight-server/control/input_handler.cpp
@@ -69,9 +69,8 @@ std::shared_ptr<events::JoypadTypes> create_new_joypad(const events::StreamSessi
     case NINTENDO:
       final_type = wolf::config::ControllerType::NINTENDO;
       break;
-    case UNKNOWN:
-    case AUTO:
-      final_type = wolf::config::ControllerType::AUTO;
+    case ULTIMATE2:
+      final_type = wolf::config::ControllerType::ULTIMATE2;
       break;
     default:
       final_type = wolf::config::ControllerType::AUTO;
@@ -135,7 +134,7 @@ std::shared_ptr<events::JoypadTypes> create_new_joypad(const events::StreamSessi
     auto result = Ultimate2Joypad::create({.name = "8BitDo Ultimate 2 (virtual) pad",
                                            .vendor_id = 0x2DC8,
                                            .product_id = 0x6012,
-                                           .version = 0x0100});
+                                           .version = 0x0001});
     if (!result) {
       logs::log(logs::error, "Failed to create Ultimate 2 joypad: {}", result.getErrorMessage());
       return {};
@@ -711,8 +710,8 @@ void controller_motion(const CONTROLLER_MOTION_PACKET &pkt, events::StreamSessio
       if (pkt.motion_type == ACCELERATION) {
         std::get<Ultimate2Joypad>(*selected_pad).set_motion(inputtino::Ultimate2Joypad::ACCELERATION, x, y, z);
       } else if (pkt.motion_type == GYROSCOPE) {
-        std::get<Ultimate2Joypad>(*selected_pad)
-            .set_motion(inputtino::Ultimate2Joypad::GYROSCOPE, deg2rad(x), deg2rad(y), deg2rad(z));
+        std::get<Ultimate2Joypad>(*selected_pad).set_motion(inputtino::Ultimate2Joypad::GYROSCOPE, x, y, z);
+
       }
     }
   }
